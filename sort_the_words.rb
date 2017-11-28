4db153c988315a24a94d2aeafd07d92076726c48
Olivia Boertje <malevolentninja@users.noreply.github.com>
2017-11-28 17:30:31 +0000

    Create sort_the_words.rb

---
+++ sort_the_words.rb
@@ -0,0 +1,13 @@
+=begin
+
+Problem Statement
+Create a method named 'sort_string' which accepts a String and rearranges all the words in ascending order, by length.
+Let's not treat the punctuation marks any different than other characters 
+assume that we will always have single space to separate the words.
+
+=end
+
+
+def sort_string(string)
+  string.split(' ').sort{|x, y| x.length <=> y.length}.join(' ')
+end
